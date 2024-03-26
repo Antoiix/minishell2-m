284572de904077510bb47499984c91e20e233c95
Antoine Orange <antoine.orange@epitech.eu>
2024-03-26 05:13:59 +0100

    test: fix tests semicolon

--- tests/test_semicolon.c
+++ tests/test_semicolon.c
@@ -12,8 +12,6 @@
 Test(semicolon, test_ls)
 {
     cr_redirect_stdout();
-    system("echo \"cd include/ ; ls -l ; ls -l | wc -l\" | ./mysh");
-    cr_assert_stdout_eq_str("total 4\n"
-                            "-rw-r--r--. 1 antoiix antoiix 2035 Mar 26 01:02 my.h\n"
-                            "2\n");
+    system("echo \"cd include/ ; ls -l | wc -l\" | ./mysh");
+    cr_assert_stdout_eq_str("2\n");
 }
